dc01a2556c5ce8d76f888fa499fb309f57c755bd
Joby-John <joby432john@gmail.com>
2024-09-08 14:44:55 +0530

    did the variables

---
+++ variable/index.js
@@ -0,0 +1,9 @@
+// // declaration:- let x;
+
+let fullName= "Joby John";
+let age = 25;
+let inCollege = true;
+
+document.getElementById("p1").textContent = `your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `you are in college: ${inCollege}`;
